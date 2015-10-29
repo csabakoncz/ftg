f76afb3557308e62af3ef7b83f7537a6eab4ca26
Csaba Koncz <csabakoncz@gmail.com>
2015-10-29 22:29:19 +0100

    App warns of unsaved data when navigatig off the page

--- v3/app/controllers/appController.js
+++ v3/app/controllers/appController.js
@@ -1,28 +1,25 @@
 define([ '../ngmodule' ], function(appModule) {
-    appModule.controller('appController', function($scope, entityConfig, userService, loggerService) {
+    appModule.controller('appController', function($scope, entityConfig, userService, loggerService, $window) {
 
         loggerService.scope = $scope;
-        
+
         $scope.entityConfig = entityConfig;
         $scope.entities = entityConfig.entities;
-        
-        $scope.appUserInfo={};
-        
-        $scope.updateUserInfo=function(){
+
+        $scope.appUserInfo = {};
+
+        $scope.updateUserInfo = function() {
             userService.updateUserInfo($scope.appUserInfo);
         }
 
-        //this function is used as a Parse callback, too
-        $scope.updateUserInfoNonAngular=function(){
+        // this function is used as a Parse callback, too
+        $scope.updateUserInfoNonAngular = function() {
             $scope.$apply($scope.updateUserInfo);
         }
         $scope.updateUserInfo();
-        
+
         $scope.editing = {};
 
-        $scope.previewWidth=400;
-        $scope.previewHeight=400;
-        
         $scope.statusInfo = function(msg) {
             $scope.error = false;
             $scope.status = msg;
@@ -31,11 +28,26 @@ define([ '../ngmodule' ], function(appModule) {
             $scope.error = true;
             $scope.status = msg;
         }
-        
+
         $scope.statusInfo('OK');
-        
-        $scope.$on('exceptionOccurred', function(event, exception, cause){
-           $scope.statusError('BIG PROBLEM: '+exception+': '+cause); 
+
+        $scope.$on('exceptionOccurred', function(event, exception, cause) {
+            $scope.statusError('BIG PROBLEM: ' + exception + ': ' + cause);
         });
+
+        var checkUnsavedObjects = function() {
+            var dirtyEntityCollection = entityConfig.entities.find(function(entity) {
+                var dirtyItem = entity.items.find(function(entityItem) {
+                    return entityItem.dirty();
+                });
+                return dirtyItem != undefined;
+            });
+
+            if (dirtyEntityCollection) {
+                return "There are unsaved entries. Are you sure you want to leave without saving them?";
+            }
+        }
+
+        $window.onbeforeunload = checkUnsavedObjects;
     });
 });
